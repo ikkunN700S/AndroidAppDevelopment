43a65d600594ec97b8e257fa31af0c4319d6eaf5
Ibuki Yamada <135494855+ikkunN700S@users.noreply.github.com>
2024-09-03 15:27:15 +0900

    ・セーブボタン追加 ・データストア実装 ・テキストセンターぞろえ

--- app/src/main/java/jp/ac/meijou/android/s231205183/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205183/MainActivity.java
@@ -3,6 +3,8 @@ package jp.ac.meijou.android.s231205183;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -17,6 +19,7 @@ import jp.ac.meijou.android.s231205183.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +29,55 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        prefDataStore = PrefDataStore.getInstance(this);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        binding.text.setText(R.string.text2);
+        // ボタンクリック動作
+        binding.button.setOnClickListener(view ->{
+            // テキスト指定
+            // binding.text.setText(R.string.text2);
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
 
-        var icon = ContextCompat.getDrawable(this, R.drawable.ic_android_black_24dp);
-        binding.imageView2.setImageDrawable(icon);
+            // 画像指定
+            var icon = ContextCompat.getDrawable(this, R.drawable.ic_android_red_24dp);
+            binding.imageView.setImageDrawable(icon);
+        });
+
+        binding.button2.setOnClickListener((view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        }));
+
+        // editTextTextが更新されたらテキストを変更する
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent((name -> binding.text.setText(name)));
     }
 }
