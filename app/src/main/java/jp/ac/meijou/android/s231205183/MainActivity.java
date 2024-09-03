69e7c392702d9497397179ed0a2bd6cc597ef191
Ibuki Yamada <135494855+ikkunN700S@users.noreply.github.com>
2024-09-03 11:46:27 +0900

    ・テキストのid設定 ・色及びサイズ変更

--- app/src/main/java/jp/ac/meijou/android/s231205183/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205183/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s231205183;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s231205183.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.text.setText(R.string.text2);
     }
 }
